285159247134168ab0f76d5fa57b8b83ba0f5f72
Wiktor Jezioro <wikjezioro@op.pl>
2016-11-12 11:40:24 +0100

    Update downloader.js

--- bin/actions/downloader.js
+++ bin/actions/downloader.js
@@ -71,7 +71,7 @@ Downloader.prototype._validate = function() {
  */
 Downloader.prototype._stripISOString = function(fileName){
   var re = /^\(\d{4}-\d{2}-\d{2}T\d{2};\d{2};\d{2}.\d{3}Z\)-/g;
-  return fileName.replace(re, '');
+  return fileName.toString().replace(re, '');
 };
 
 /**
